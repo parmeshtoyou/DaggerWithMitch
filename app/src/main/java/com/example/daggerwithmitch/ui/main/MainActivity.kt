26db49340ea991a031cab2642c3aabdb60636c3b
parmeshtoyou <parmeshtoyou@gmail.com>
2020-04-21 08:47:45 +0530

    clean-up

--- app/src/main/java/com/example/daggerwithmitch/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/daggerwithmitch/ui/main/MainActivity.kt
@@ -11,9 +11,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
